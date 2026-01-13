48749c6a7c84b5d3dc8c0d5320c7dbbac984f9aa
Sohoo122 <sooho040@naver.com>
2026-01-13 21:42:57 +0900

    chore: 배경화면 색 수정

--- lib/common/constants/app_constants.dart
+++ lib/common/constants/app_constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
   static const double designWidth = 393.9;
   static const double designHeight = 852.49;
-  static const int backgroundColor = 0xFFDF8E9;
+  static const int backgroundColor = 0xFFFDF8E9;
 }
