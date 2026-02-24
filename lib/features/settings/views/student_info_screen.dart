f092474777d4f6d97a1902fad0db55afb57fe423
Juwhan Jung (Isaac) <juwhani0309@gmail.com>
2026-02-23 22:58:08 -0500

    Implemented minimal elements in student info screen

--- lib/features/settings/views/student_info_screen.dart
+++ lib/features/settings/views/student_info_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:front_end/features/home/utils/index.dart';
+import 'package:front_end/features/settings/views/widgets/student_info_widgets.dart';
+
 
 class StudentInfoScreen extends StatefulWidget {
   const StudentInfoScreen({super.key});
@@ -10,6 +15,27 @@ class StudentInfoScreen extends StatefulWidget {
 class _StudentInfoScreenState extends State<StudentInfoScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color(AppConstants.backgroundColor),
+      body: SafeArea(
+        child: Column(
+          children: [
+            StudentInfoHeader(),
+
+            SizedBox(height: 16.h,),
+
+            StudentInfoInfoTab(),
+
+            SizedBox(height: 24.h,),
+
+            StudentInfoCard(studentName: "홍시아"),
+
+            SizedBox(height: 24.h,),
+
+            StudentInfoCard(studentName: "홍서준"),
+          ],
+        ),
+      ),
+    );
   }
 }
