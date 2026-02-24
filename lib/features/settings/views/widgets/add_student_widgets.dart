47661f812b1a703718ac6180f7f1d5c1a74f2a95
Juwhan Jung (Isaac) <juwhani0309@gmail.com>
2026-02-23 22:24:39 -0500

    Implemented minimal functions except for school info

--- lib/features/settings/views/widgets/add_student_widgets.dart
+++ lib/features/settings/views/widgets/add_student_widgets.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 // ======== STATELESS WIDGETS ========
 // 자녀 추가 헤더
@@ -8,35 +9,40 @@ class AddStudentHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 56.h,
-      padding: EdgeInsets.all(16.h),
+    return GestureDetector(
+      onTap: () {
+        Navigator.pop(context);
+      },
+      child: Container(
+        width: double.infinity,
+        height: 56.h,
+        padding: EdgeInsets.all(16.h),
 
-      decoration: BoxDecoration(
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withValues(alpha: 0.03),
-            blurRadius: 10,
-            offset: const Offset(0, 4),
-          ),
-        ],
-      ),
-
-      child: Row(
-        children: [
-          Text("icon"),
-
-          Text(
-            '설정',
-            style: TextStyle(
-              fontSize: 16.h,
-              fontWeight: FontWeight.w400,
-              color: Color(0xFF1C4B2F),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withValues(alpha: 0.03),
+              blurRadius: 10,
+              offset: const Offset(0, 4),
             ),
-          ),
-        ],
+          ],
+        ),
+
+        child: Row(
+          children: [
+            SvgPicture.asset('assets/icons/back_arrow.svg', width: 24.h, height: 24.h),
+
+            Text(
+              '자녀 추가',
+              style: TextStyle(
+                fontSize: 16.h,
+                fontWeight: FontWeight.w400,
+                color: Color(0xFF1C4B2F),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -50,10 +56,11 @@ class AddStudentInfoTab extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: 360.w,
-      padding: EdgeInsets.all(16.w),
+      height: 108.h,
+      padding: EdgeInsets.all(20.w),
 
       decoration: BoxDecoration(
-        color: Color(0xFFE0ECE4),
+        color: Color(0xFFBFD7C8),
         borderRadius: BorderRadius.circular(16.r),
         boxShadow: [
           BoxShadow(
@@ -65,13 +72,37 @@ class AddStudentInfoTab extends StatelessWidget {
       ),
 
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        
         children: [
-          Row(
-            children: [
-              Text("icon"),
+          SizedBox(
+            width: 320.w,
+            height: 40.h,
 
-              Text("자녀 정보 입력"),
-            ],
+            child: Row(
+              children: [
+                Container(
+                  width: 40.h, // Same value with height => to make it perfect square
+                  height: 40.h,
+                  padding: EdgeInsets.all(10.h),
+                  decoration: BoxDecoration(
+                    color: Color(0x33FFFFFF),
+                    borderRadius: BorderRadius.circular(16.r),
+                  ),
+
+                  child: SvgPicture.asset('assets/icons/personal_white.svg', width: 24.h, height: 24.h),
+                ),
+
+                Text(
+                  "자녀 정보 입력",
+                  style: TextStyle(
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ],
+            ),
           ),
 
           Text("자녀의 학교, 생년월일, 이름을 정확히 입력해주세요"),
@@ -107,7 +138,14 @@ class GetName extends StatelessWidget {
         children: [
           Row(
             children: [
-              Text("Icon"),
+              Container(
+                width: 20.h, // Same value with height => to make it perfect square
+                height: 20.h,
+                padding: EdgeInsets.all(2.h),
+                decoration: BoxDecoration(),
+
+                child: SvgPicture.asset('assets/icons/personal_green.svg', width: 16.h, height: 16.h),
+              ),
 
               Text(
                 "이름",
@@ -172,7 +210,14 @@ class GetBirthDate extends StatelessWidget {
         children: [
           Row(
             children: [
-              Text("Icon"),
+              Container(
+                width: 20.h, // Same value with height => to make it perfect square
+                height: 20.h,
+                padding: EdgeInsets.all(2.h),
+                decoration: BoxDecoration(),
+
+                child: SvgPicture.asset('assets/icons/calendar.svg', width: 16.h, height: 16.h),
+              ),
 
               Text(
                 "생년월일",
@@ -196,8 +241,6 @@ class GetBirthDate extends StatelessWidget {
               textAlignVertical: TextAlignVertical.center,
               readOnly: true,
               decoration: InputDecoration(
-                hintText: '생년월일 선택',
-
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(8.w),
                   borderSide: BorderSide.none,
@@ -253,7 +296,14 @@ class GetSchool extends StatelessWidget {
         children: [
           Row(
             children: [
-              Text("Icon"),
+              Container(
+                width: 20.h, // Same value with height => to make it perfect square
+                height: 20.h,
+                padding: EdgeInsets.all(2.h),
+                decoration: BoxDecoration(),
+
+                child: SvgPicture.asset('assets/icons/school.svg', width: 16.h, height: 16.h),
+              ),
 
               Text(
                 "학교",
@@ -279,29 +329,37 @@ class AddStudentButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      width: 360.w,
-      padding: EdgeInsets.all(16.w),
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        borderRadius: BorderRadius.circular(16.w),
+        onTap: () {},
+        child: Ink(
+          width: 360.w,
+          padding: EdgeInsets.all(16.w),
 
-      decoration: BoxDecoration(
-        color: Color(0xFFE0ECE4),
-        borderRadius: BorderRadius.circular(16.r),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withValues(alpha: 0.05),
-            blurRadius: 10,
-            offset: const Offset(0, 2),
+          decoration: BoxDecoration(
+            color: Color(0xFFE0ECE4),
+            borderRadius: BorderRadius.circular(16.r),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withValues(alpha: 0.05),
+                blurRadius: 10,
+                offset: const Offset(0, 2),
+              ),
+            ],
           ),
-        ],
-      ),
 
-      child: Text(
-        "자녀 추가하기",
-        style: TextStyle(
-          fontSize: 18.sp,
-          fontWeight: FontWeight.w500,
-          color: Color(0xFF0A0A0A),
+          child: Center(
+            child: Text(
+              "자녀 추가하기",
+              style: TextStyle(
+                fontSize: 18.sp,
+                fontWeight: FontWeight.w500,
+                color: Color(0xFF0A0A0A),
+              ),
+            ),
+          ),
         ),
       ),
     );
@@ -314,29 +372,37 @@ class CancelButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      width: 360.h,
-      padding: EdgeInsets.all(16.w),
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        borderRadius: BorderRadius.circular(16.w),
+        onTap: () {},
+        child: Ink(
+          width: 360.h,
+          padding: EdgeInsets.all(16.w),
 
-      decoration: BoxDecoration(
-        color: Color(0xFFFFFFFF),
-        borderRadius: BorderRadius.circular(16.r),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withValues(alpha: 0.05),
-            blurRadius: 10,
-            offset: const Offset(0, 2),
+          decoration: BoxDecoration(
+            color: Color(0xFFFFFFFF),
+            borderRadius: BorderRadius.circular(16.r),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withValues(alpha: 0.05),
+                blurRadius: 10,
+                offset: const Offset(0, 2),
+              ),
+            ],
           ),
-        ],
-      ),
 
-      child: Text(
-        "취소",
-        style: TextStyle(
-          fontSize: 18.sp,
-          fontWeight: FontWeight.w500,
-          color: Color(0xFF0A0A0A),
+          child: Center(
+            child: Text(
+              "취소",
+              style: TextStyle(
+                fontSize: 18.sp,
+                fontWeight: FontWeight.w500,
+                color: Color(0xFF0A0A0A),
+              ),
+            ),
+          ),
         ),
       ),
     );
