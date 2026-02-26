874267f87f1a1a12c841351daafd31c819e82ecc
Juwhan Jung (Isaac) <juwhani0309@gmail.com>
2026-02-25 23:04:25 -0500

    feat: Implemented CreateChatroom Button

--- lib/features/home/views/widgets/home_teacher_body.dart
+++ lib/features/home/views/widgets/home_teacher_body.dart
@@ -8,59 +8,96 @@ class TeacherHomeBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          TeacherHomeHeader(),
+    return Stack(
+      children: [
+        // 화면 영역 정의 - 채팅방 생성 버튼 위치를 지정하기 위한 기준 역할
+        SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+        ),
 
-          SizedBox(height: 16.h),
+        // 학급 채팅방 리스트
+        SingleChildScrollView(
+          child: Column(
+            children: [
+              TeacherHomeHeader(),
 
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16.w),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // 채팅 카드
-                ChatCard(backgColor: 0xFFF8E9A6, fontColor: 0xFF26643E,),
+              SizedBox(height: 16.h),
 
-                SizedBox(height: 16.h),
-
-                // 채팅방 리스트 타이틀
-                Text(
-                  '개설된 채팅방 리스트',
-                  style: TextStyle(
-                    fontSize: 16.sp,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-
-                SizedBox(height: 16.h),
-
-                // 방 리스트
-                Column(
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.w),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    ClassroomCard(
-                      schoolYear: 2025,
-                      grade: 1,
-                      section: '2',
-                      chatroomCode: '6DK4D1',
-                    ),
+                    // 채팅 카드
+                    ChatCard(backgColor: 0xFFF8E9A6, fontColor: 0xFF26643E,),
+
                     SizedBox(height: 16.h),
-                    ClassroomCard(
-                      schoolYear: 2024,
-                      grade: 1,
-                      section: '1',
-                      chatroomCode: '47JP4K',
+
+                    // 채팅방 리스트 타이틀
+                    Text(
+                      '개설된 채팅방 리스트',
+                      style: TextStyle(
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.w400,
+                      ),
                     ),
+
                     SizedBox(height: 16.h),
+
+                    // 방 리스트
+                    Column(
+                      children: [
+                        ClassroomCard(
+                          schoolYear: 2025,
+                          grade: 1,
+                          section: '2',
+                          chatroomCode: '6DK4D1',
+                        ),
+                        SizedBox(height: 16.h),
+                        ClassroomCard(
+                          schoolYear: 2024,
+                          grade: 1,
+                          section: '1',
+                          chatroomCode: '47JP4K',
+                        ),
+                        SizedBox(height: 16.h),
+                      ],
+                    ),
+
                   ],
                 ),
-
-              ],
+              ),
+            ],
+          ),
+        ),
+        
+        // 채팅방 생성 버튼
+        Positioned(
+          right: 16.w,
+          bottom: 16.h,
+          child: Material(
+            color: Colors.transparent,
+            child: InkWell(
+              borderRadius: BorderRadius.circular(32.r),
+              onTap: () {},
+              child: Ink(
+                width: 64.w,
+                height: 64.h,
+                decoration: BoxDecoration(
+                  color: Color(0xFFF8E9A6),
+                  shape: BoxShape.circle,
+                ),
+                child: Icon(
+                  CupertinoIcons.add,
+                  size: 32.0,
+                  color: Color(0xFFFFFFFF),
+                ),
+              ),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
